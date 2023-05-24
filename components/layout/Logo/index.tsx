aa7ed608f0c5c8b38f9857735aba6fef40cc0452
Nico <nicolas.andre.pf@gmail.com>
2023-05-24 16:07:11 +0800

    Changed the logo

--- components/layout/Logo/index.tsx
+++ components/layout/Logo/index.tsx
@@ -5,7 +5,7 @@ function Logo() {
 	return (
 		<div className="font-oswald ml-4 flex items-center text-lg font-bold uppercase text-gray-300 hover:text-white md:ml-12 md:text-3xl">
 			<FaJournalWhills />
-			<span className="ml-4 hidden md:flex">Deemo&apos;s Journal</span>
+			<span className="ml-4 hidden md:flex">Kneeco&apos;s Journal</span>
 		</div>
 	);
 }
